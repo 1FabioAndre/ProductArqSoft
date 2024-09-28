26d463dd7520a8265a43bed2de47b08846cb48ca
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-27 21:55:59 -0400

    Se puso los codigos de estado correspondientes al product controller

--- src/main/java/com/example/product/ProductController.java
+++ src/main/java/com/example/product/ProductController.java
@@ -1,27 +1,29 @@
 package com.example.product.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
 
     @PostMapping
-    public String create() {
-        return "Created";
+    public ResponseEntity<String> create() {
+        return new ResponseEntity<>("Created", HttpStatus.CREATED);
     }
 
     @GetMapping
-    public String obtain() {
-        return "The product";
+    public ResponseEntity<String> obtain() {
+        return new ResponseEntity<>("The product", HttpStatus.OK);
     }
 
     @DeleteMapping
-    public String delete() {
-        return "Deleted";
+    public ResponseEntity<String> delete() {
+        return new ResponseEntity<>("Deleted", HttpStatus.NO_CONTENT);
     }
 
     @PutMapping
-    public String update() {
-        return "Updated";
+    public ResponseEntity<String> update() {
+        return new ResponseEntity<>("Updated", HttpStatus.OK);
     }
 }
