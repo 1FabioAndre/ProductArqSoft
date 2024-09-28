6cd44a23e3411c75c1648170d14e216829195224
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-27 21:55:22 -0400

    Se creo el product controller

---
+++ src/main/java/com/example/product/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.product.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProductController {
+
+    @PostMapping
+    public String create() {
+        return "Created";
+    }
+
+    @GetMapping
+    public String obtain() {
+        return "The product";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "Deleted";
+    }
+
+    @PutMapping
+    public String update() {
+        return "Updated";
+    }
+}
