292adf6f9220405506bbbb4910ff64c05fc51f75
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-27 22:49:29 -0400

    Paso 6 del doc exeption

--- src/main/java/com/example/product/GetProductService.java
+++ src/main/java/com/example/product/GetProductService.java
@@ -2,13 +2,12 @@ package com.example.product.service;
 
 import com.example.product.IProductRepository;
 import com.example.product.Query;
+import com.example.product.exception.ProductNotFoundException;
 import com.example.product.model.Product;
 import com.example.product.model.ProductDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class GetProductService implements Query<Integer, ProductDto> {
@@ -25,6 +24,6 @@ public class GetProductService implements Query<Integer, ProductDto> {
             return ResponseEntity.status(HttpStatus.OK).body(
                     product.map(ProductDto::new).get());
         }
-        throw new RuntimeException("Product Id doesn't exist.");
+        throw new ProductNotFoundException();
     }
 }
